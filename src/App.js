e3e69394f47c1cc2bb0556eeda3c587088fb9cd1
Leo Xu <leo.xu.64@gmail.com>
2021-02-17 16:28:06 -0500

    Added map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
 import './App.css';
+import "./styles.css";
+import MapChart from "./MapChart";
 
 function App() {
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Map</h1>
+      <div class="container">
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
+      </div>
     </div>
   );
 }
