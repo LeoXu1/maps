78166aa093456df053578b70dd4ff81bdf4acdcc
Leo Xu <leo.xu.64@gmail.com>
2023-12-22 16:13:37 +0000

    package updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App'
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
